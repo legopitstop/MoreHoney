6e1a04a9ca977dfcb1388933a98acfddc0f00ef0
Legopitstop <1589lego@gmail.com>
2024-12-17 16:46:55 -0600

    setup

---
+++ fabric/src/main/java/dev/lpsmods/morehoney/MoreHoney.java
@@ -0,0 +1,11 @@
+package dev.lpsmods.morehoney;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MoreHoney implements ModInitializer {
+    
+    @Override
+    public void onInitialize() {
+        Bootstrap.init();
+    }
+}
